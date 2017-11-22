aacaa160dad72cad7c75a1cb394c4901d1e16ffd
Dave Ellis <dave@ellisnetmail.co.uk>
2017-11-22 20:19:30 +0000

    Added code for A.1, A.2 and A.3

--- homework.rb
+++ homework.rb
@@ -6,10 +6,16 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 
 # 1. Add `"Edinburgh Waverley"` to the end of the array
+stops.push("Edinburgh Waverley")
+p stops
 
 # 2. Add `"Glasgow Queen St"` to the start of the array
+stops.unshift("Glasgow Queen St")
+p stops
 
 # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+stops.insert(4,"Polmont")
+p stops
 
 # 4. Work out the index position of `"Linlithgow"`
 
